98120a9a3a8d132be74e5ce62e55df7245299ea6
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-03-19 14:46:24 +0300

    feat : add responseEntity class to modify the response status

--- src/main/java/com/antonio/hello_spring/controller/HelloController.java
+++ src/main/java/com/antonio/hello_spring/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.antonio.hello_spring.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello-world")
-    public String helloWorld () {
+    public String helloWorld() {
         return "Hello world";
     }
 
     @GetMapping("/welcome")
-    public String welcome (@RequestParam String name) {
-        return "Welcome " + name;
+    public ResponseEntity<String> welcome(@RequestParam String name) {
+        if (name == null || name.isBlank()) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("Parameter name is required");
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .header("Content-type", "text/plain")
+                    .body("Welcome " + name);
+        }
     }
 }
