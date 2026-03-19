2b58e577f6fbcedd2616e9485fdfd8955cf8b495
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-03-19 11:40:25 +0300

    feat : create Student model

---
+++ src/main/java/com/antonio/hello_spring/model/Student.java
@@ -0,0 +1,50 @@
+package com.antonio.hello_spring.model;
+
+public class Student {
+    private String reference;
+    private String firstname;
+    private String lastName;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String reference, String firstName, String lastName, int age) {
+        this.reference = reference;
+        this.firstname = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
