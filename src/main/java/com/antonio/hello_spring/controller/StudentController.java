cd38b329b8c2564e91e0536e02bb4c027027c22b
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2026-03-19 15:39:21 +0300

    feat : modify getStudents methods with adding reponse entity and conditions for text/plain and application/json

--- src/main/java/com/antonio/hello_spring/controller/StudentController.java
+++ src/main/java/com/antonio/hello_spring/controller/StudentController.java
@@ -24,17 +24,36 @@ public class StudentController {
     public ResponseEntity<List<Student>> addStudents(@RequestBody List<Student> newStudents) {
         students.addAll(newStudents);
         return ResponseEntity.status(HttpStatus.CREATED)
-            .body(students);
+                .body(students);
     }
 
     @GetMapping
-    public String getStudents(@RequestHeader(value = "Accept", defaultValue = "text/plain") String acceptHeader) {
-        if (acceptHeader.equals("text/plain")) {
-            return students.stream()
+    public ResponseEntity<?> getStudents(
+            @RequestHeader(value = "Accept", required = false) String acceptHeader) {
+        if (acceptHeader == null || acceptHeader.isBlank()) {
+            return ResponseEntity.badRequest().body("Header 'Accept' est obligatoire !");
+        }
+
+        String header = acceptHeader.toLowerCase().trim();
+
+        // acceptHeader = text/plain
+        if (header.contains("text/plain")) {
+            String result = students.stream()
                     .map(s -> s.getFirstname() + " " + s.getLastName())
                     .collect(Collectors.joining("\n"));
-        } else {
-            return "Format non supporté";
+            return ResponseEntity.ok()
+                    .header("Content-Type", "text/plain")
+                    .body(result);
         }
+
+        // acceptHeader = application/json
+        if (header.contains("application/json")) {
+            return ResponseEntity.ok()
+                    .header("Content-Type", "application/json")
+                    .body(students);
+        }
+
+        return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED)
+                .body("Format non supporté : " + acceptHeader);
     }
 }
